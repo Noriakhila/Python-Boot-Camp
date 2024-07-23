d8755800937e21f07c521f642caa84c7f1995d4b
Noriakhila <nnoriakhila@gmail.com>
2024-07-23 21:56:00 +0530

    Add files via upload

---
+++ ques_7i.py
@@ -0,0 +1,12 @@
+'''
+you are given with a space separated integer list find no of even eemnts and no of odd elements in a given list'''
+n=list(map(int,input().split(" ")))
+even_cnt=0
+odd_cnt=0
+for i in range(0,len(n)):
+    if(n[i]%2==0):
+        even_cnt+=1
+    else:
+        odd_cnt+=1
+print(even_cnt)
+print(odd_cnt)
