db69b283ab83c0ffd4fb55e7e22da545ceef866c
Noriakhila <nnoriakhila@gmail.com>
2024-07-23 22:18:30 +0530

    Update and rename hw8.py to palindrome.py

--- hw8.py
+++
@@ -1,2 +0,0 @@
-''' plaindrome'''
-num=int
---
+++ palindrome.py
@@ -0,0 +1,6 @@
+''' plaindrome'''
+def ispalindrome(str)
+for i in range(0,int(len(str)/2):
+  if str[i]!=str(len(str)-i-1]:
+    return False
+return True
